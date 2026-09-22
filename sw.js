399536682c17a218b5ea8e23192005bafc80108e
monguied-star <mail.monguied@gmail.com>
2026-09-21 19:24:08 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "finanzas-v1";
+
+const archivos = [
+"/",
+"/index.html",
+"/app.js",
+"/manifest.json"
+];
+
+self.addEventListener("install", e => {
+e.waitUntil(
+caches.open(CACHE)
+.then(cache => cache.addAll(archivos))
+);
+});
+
+self.addEventListener("fetch", e => {
+e.respondWith(
+caches.match(e.request)
+.then(resp => resp || fetch(e.request))
+);
+});
